d01a0816f41ba22aefb8b96236872b6c3ebbeabf
Reiss Johnson <reissjohnson@me.com>
2018-05-14 16:41:26 +0100

    added task to create data_migrator, added template for data_migrator migration

---
+++ lib/generators/data_migrator_generator.rb
@@ -0,0 +1,24 @@
+require 'rails/generators'
+require 'rails/generators/migration'
+
+class DataMigrator
+  class InstallGenerator < ::Rails::Generators::Base
+    include Rails::Generators::Migration
+    source_root File.expand_path('../templates', __FILE__)
+    desc "add the migrations"
+    namespace 'data_migrator'
+
+    def self.next_migration_number(path)
+      unless @prev_migration_nr
+        @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+      else
+        @prev_migration_nr += 1
+      end
+      @prev_migration_nr.to_s
+    end
+
+    def copy_migrations
+      migration_template "create_data_migrations_table.rb", "db/migrate/create_data_migrations_table.rb"
+    end
+  end
+end
